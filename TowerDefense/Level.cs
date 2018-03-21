c15676c90072b183602a941dda2950eb62769b4a
Eric <wunderkammer123@gmail.com>
2018-03-21 12:23:36 -0400

    Built out Play method and coded up a 'level' in Main.  Game now plays in most basic sense.

--- TowerDefense/Level.cs
+++ TowerDefense/Level.cs
@@ -11,13 +11,39 @@
             _invaders = invaders;
         }
 
+        //returns true if player wins, false if player loses.
         public bool Play()
         {
             //Run until all invaders are neutralized or invader reaches end
+            int remainingInvaders = _invaders.Length;
 
-            //first each tower has an opportunity to fire on invaders
+            while(remainingInvaders > 0)
+            {
+                //first each tower has an opportunity to fire on invaders
+                foreach(Tower tower in Towers)
+                {
+                    tower.FireOnInvaders(_invaders);
+                }
 
-            //Count and more the invaders that are still active
+                //Count and more the invaders that are still active
+                remainingInvaders = 0;
+
+                foreach(Invader invader in _invaders)
+                {
+                    if (invader.IsActive)
+                    {
+                        invader.Move();
+
+                        if (invader.HasScored)
+                        {
+                            return false;
+                        }
+
+                        remainingInvaders++;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
