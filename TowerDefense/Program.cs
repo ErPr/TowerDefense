46d4581ab57dc158cb3f774049e51f9e6fef5da1
Eric <wunderkammer123@gmail.com>
2018-03-21 23:40:42 -0400

    Created FastInvader class; made associated changes.

--- TowerDefense/Program.cs
+++ TowerDefense/Program.cs
@@ -27,7 +27,7 @@ namespace TowerDefense
                 Invader[] invaders =
                 {
                     new Invader(path),
-                    new Invader(path),
+                    new FastInvader(path),
                     new ShieldedInvader(path),
                     new Invader(path),
                 };
