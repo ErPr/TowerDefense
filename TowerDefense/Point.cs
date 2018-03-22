d77ae99e9ff0fa7eaccfd3b66fe7a7c20eb5d892
Eric <wunderkammer123@gmail.com>
2018-03-22 07:21:09 -0400

    Created IsOnPath method in Path.cs, added associated Equals override in Point.cs

--- TowerDefense/Point.cs
+++ TowerDefense/Point.cs
@@ -18,6 +18,18 @@ namespace TowerDefense
             return X + ", " + Y;
         }
 
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Point))
+            {
+                return false;
+            }
+
+            Point that = obj as Point;
+
+            return this.X == that.X && this.Y == that.Y;
+        }
+
         public int DistanceTo(int x, int y)
         {
             int xDiff = X - x;
